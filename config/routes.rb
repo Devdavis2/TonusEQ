93b4f58a4181dc95da08cb316c3548194f03fb8a
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-05 09:41:08 -0400

    creating heroku api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
+  # Rails.application.routes.draw do
+  root 'welcome#index'
   resources :moods
   resources :tonus_eqs, only: [:index, :show] do
     resources :moods, except: [:show]
