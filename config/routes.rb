add07f0da100acaad6db2b4d762207213130b4b5
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-05 19:34:05 -0400

    updating back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :moods
-  resources :tonus_eqs, only: [:index, :show] do
+  resources :tonus_eqs, only: [:index, :show, :create] do
     resources :moods, except: [:show]
   end
 end
