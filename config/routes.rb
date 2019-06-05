a619e68b5c2e0f68e572b54b0c6d3b6ac0ae52db
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-05 10:14:32 -0400

    updating heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # Rails.application.routes.draw do
   root 'welcome#index'
   resources :moods
   resources :tonus_eqs, only: [:index, :show] do
     resources :moods, except: [:show]
   end
-end
 end
